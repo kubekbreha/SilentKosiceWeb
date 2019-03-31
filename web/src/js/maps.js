014ada9737d9a75e39af0d6006a7d957a8102cd6
Richard Babjak <richard.babjak@ibm.com>
2019-03-31 07:40:32 +0200

    upravene 2

--- web/src/js/maps.js
+++ web/src/js/maps.js
@@ -14,7 +14,7 @@ async function initMap() {
                 Object.keys(data.val()[d].records).forEach(b => {
                     let datas = data.val()[d].records[b];
                     console.log(datas.lat);
-                    let obj = {location: new google.maps.LatLng(datas.lat, datas.long), weight: datas.decibel};
+                    let obj = {location: new google.maps.LatLng(datas.lat, datas.long), weight: datas.decibel/4};
                     mapData.push(obj);
 
                     let gps = new google.maps.LatLng(datas.lat, datas.long);
@@ -221,10 +221,27 @@ async function initMap() {
                 // 'rgba(255, 0, 0, 1)'
             ];
 
+        // var gradient = [
+        //     'rgba(0, 255, 255, 0)',
+        //     'rgba(0, 255, 255, 1)',
+        //     'rgba(0, 191, 255, 1)',
+        //     'rgba(0, 127, 255, 1)',
+        //     'rgba(0, 63, 255, 1)',
+        //     'rgba(0, 0, 255, 1)',
+        //     'rgba(0, 0, 223, 1)',
+        //     'rgba(0, 0, 191, 1)',
+        //     'rgba(0, 0, 159, 1)',
+        //     'rgba(0, 0, 127, 1)',
+        //     'rgba(63, 0, 91, 1)',
+        //     'rgba(127, 0, 63, 1)',
+        //     'rgba(191, 0, 31, 1)',
+        //     'rgba(255, 0, 0, 1)'
+        // ]
+
             heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
-            heatmap.set('radius', heatmap.get('radius') ? null : 10);
-            heatmap.set('maxIntensity', heatmap.get('maxIntensity') ? null : 40);
-            // heatmap.set('dissipating', heatmap.get('dissipating') ? null : true);
+            heatmap.set('radius', heatmap.get('radius') ? null : 50);
+            heatmap.set('maxIntensity', heatmap.get('maxIntensity') ? null : 10);
+            heatmap.set('dissipating', heatmap.get('dissipating') ? null : true);
 
             heatmap.setMap(map);
 
